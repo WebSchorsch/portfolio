96d8fb4067490e9254ff9eb204a3bd7d55322a9e
WebSchorsch <118896050+WebSchorsch@users.noreply.github.com>
2024-09-06 10:50:24 +0200

    Add files via upload

---
+++ css/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const radios = document.querySelectorAll('input[name="image"]');
+    const avatarImage = document.querySelector('.avatare');
+
+    radios.forEach(radio => {
+        radio.addEventListener('change', function() {
+            if (this.checked) {
+                avatarImage.src = this.value;
+            }
+        });
+    });
+});
